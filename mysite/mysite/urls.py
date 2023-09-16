4d0cb2653d4fcc71a7ed54a1a139880d26ad6c04
Dawso125 <dawson.davis@selu.edu>
2023-09-16 13:58:29 -0500

    Worked on the file structure, Added URLs

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+from .views import landing_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', landing_view)
 ]
