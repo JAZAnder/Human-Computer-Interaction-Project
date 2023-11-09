2e09d08a88e8218c54c30515bfb07bb33f26b883
CaptainSpatula01 <brycenorris2002@gmail.com>
2023-11-09 15:50:37 -0600

    Static folder and CSS
    Added the ability to create a separate CSS file inside of the static folder to customize the webpages

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "bootstrap4"
 ]
 
 MIDDLEWARE = [
@@ -113,10 +112,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
